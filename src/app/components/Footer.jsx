66a357dd7ec9476ca1fe8eba1a077a17a93d5fae
mayankchugh17 <mayankchugh222@gmail.com>
2026-03-13 13:07:00 +0530

    Footer has beenn build

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,55 +1,137 @@
 "use client";
 import Image from "next/image";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import CallIcon from '@mui/icons-material/Call';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CallIcon from "@mui/icons-material/Call";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { useState } from "react";
 
 function Footer() {
+  const [services, setServices] = useState(false);
+  const [privacy, setPrivacy] = useState(false);
+  const [account, setAccount] = useState(false);
+
   return (
-    <footer className="mx-auto pt-15 w-full flex flex-col px-5 bg-[#3C3C3C] text-gray-400 py-4 text-center gap-8">
-      <div className="flex flex-col md:flex-row justify-between gap-6">
-        <Image
-          src="/footer/logo6.webp"
-          className="w-30 md:w-40 text-white h-auto object-cover cursor-pointer"
-          alt="Logo"
-          width={250}
-          height={50}
-        />
-        <div className="flex justify-start items-center gap-4">
-            <LocationOnIcon />
-            <h1 className="text-start">Wooster parck chowk bazzar new york </h1>
-        </div>
-        <div className="flex justify-start items-center gap-4">
-            <CallIcon />
-            <div className="flex flex-col">
-            <h2 className="text-start">+1 (123) 456-7890</h2>
-            <h2 className="text-start">support@spacingtech.com</h2>
+    <footer className="bg-[#3C3C3C] w-full text-white py-16 px-5 md:px-20">
+      <div className="mx-auto">
+        <div className="grid md:grid-cols-4 gap-4 md:gap-10 ">
+          <div className="flex flex-col gap-8 text-sm md:text-xl text-[#7F7F7F]">
+            <Image
+              src="/footer/logo6.webp"
+              alt="logo"
+              width={260}
+              height={60}
+              className="w-40"
+            />
 
-
+            <div className="flex gap-3 text-md md:text-xl">
+              <LocationOnIcon sx={{ color: "#CD7752" }} />
+              <p>Wooster parck chowk bazzar new York</p>
             </div>
-        </div>
 
-      </div>
-      <div>
-        <ul className="flex flex-col items-start justify-center gap-6 font-bold">
-          <div className="flex justify-between gap-35">
-            <li>Services </li>
-            <KeyboardArrowDownIcon />
+            <div className="flex gap-3 text-md md:text-xl">
+              <CallIcon sx={{ color: "#CD7752" }} />
+              <div>
+                <p className="hover:text-[#CD7752] cursor-pointer">
+                  +1 (123) 456-7890
+                </p>
+                <p className="hover:text-[#CD7752] cursor-pointer">
+                  support@spacingtech.com
+                </p>
+              </div>
+            </div>
           </div>
-          <div className="flex justify-between gap-21">
-            <li>Privacy & terms </li>
-            <KeyboardArrowDownIcon />
+
+          {/* SERVICES */}
+          <div className=" pt-4 md:pt-0">
+            <div
+              onClick={() => setServices(!services)}
+              className="flex justify-between items-center cursor-pointer md:cursor-default"
+            >
+              <h1 className="text-lg md:text-2xl font-bold ">Services</h1>
+              <span className="md:hidden">
+                <KeyboardArrowDownIcon />
+              </span>
+            </div>
+
+            <div
+              className={`flex flex-col gap-2 md:gap-4 mt-4 text-sm md:text-[22px] text-[#7F7F7F]
+              ${services ? "block" : "hidden"} md:flex`}
+            >
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                About vegist
+              </p>
+              <p className="hover:text-[#CD7752] cursor-pointer">Faq's</p>
+              <p className="hover:text-[#CD7752] cursor-pointer">Contact us</p>
+              <p className="hover:text-[#CD7752] cursor-pointer">News</p>
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                Store location
+              </p>
+            </div>
           </div>
-          <div className="flex justify-between gap-28">
-            <li>My account </li>
-            <KeyboardArrowDownIcon />
+
+          {/* PRIVACY */}
+          <div className=" pt-4 md:pt-0">
+            <div
+              onClick={() => setPrivacy(!privacy)}
+              className="flex justify-between items-center cursor-pointer md:cursor-default"
+            >
+              <h1 className="text-lg md:text-2xl font-bold ">Privacy & Terms</h1>
+              <span className="md:hidden">
+                <KeyboardArrowDownIcon />
+              </span>
+            </div>
+
+            <div
+              className={`flex flex-col gap-2 md:gap-4 mt-4 text-sm md:text-[22px] text-[#7F7F7F]
+              ${privacy ? "block" : "hidden"} md:flex`}
+            >
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                Payment policy
+              </p>
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                Private policy
+              </p>
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                Return policy
+              </p>
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                Shipping policy
+              </p>
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                Terms & conditions
+              </p>
+            </div>
           </div>
-        </ul>
-        <div>
-          <p>&copy; 2024 My Portfolio. All rights reserved.</p>
+
+          {/* ACCOUNT */}
+          <div className=" pt-4 md:pt-0">
+            <div
+              onClick={() => setAccount(!account)}
+              className="flex justify-between items-center cursor-pointer md:cursor-default"
+            >
+              <h1 className="text-lg md:text-2xl font-bold ">My Account</h1>
+              <span className="md:hidden">
+                <KeyboardArrowDownIcon />
+              </span>
+            </div>
+
+            <div
+              className={`flex flex-col gap-2 md:gap-4 mt-4 text-[#7F7F7F] text-sm md:text-[22px]
+              ${account ? "block" : "hidden"} md:flex`}
+            >
+              <p className="hover:text-[#CD7752] cursor-pointer">My account</p>
+              <p className="hover:text-[#CD7752] cursor-pointer">My cart</p>
+              <p className="hover:text-[#CD7752] cursor-pointer">
+                Order history
+              </p>
+              <p className="hover:text-[#CD7752] cursor-pointer">My wishlist</p>
+              <p className="hover:text-[#CD7752] cursor-pointer">My address</p>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
   );
 }
+
 export default Footer;
